18878f07a95f7c29827402a0526dd4bf6bad708a
harikrishnalhk <144059589+harikrishnalhk@users.noreply.github.com>
2024-04-12 15:28:57 +0530

    Add files via upload

---
+++ portfoliojs.js
@@ -0,0 +1,11 @@
+
+let vrreadmoreel = document.getElementById("vrreadmore");
+
+
+    function readmore(){
+        window.location.href="https://vrlhk.ccbp.tech/";
+
+}
+function readmorefm(){
+    window.location.href="https://lhkfoodmunch.ccbp.tech/";
+}
